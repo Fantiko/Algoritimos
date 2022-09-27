e98605171624c6bffa1703098be037188b01039e
lapaCaio <lapacaio2001@gmail.com>
2022-09-26 22:34:14 -0300

    tentativa da função de inserção
    base para função de inserção...

--- arvore patricia.c
+++ arvore patricia.c
@@ -24,12 +24,38 @@ Arvore* insere(Arvore*a, char c[8]){
         a->tipo_no = 0;
         a->esq = a->dir = NULL;
     }else{
-        if(v < a->info){
-            a->esq = insere(a->esq, v);
-        }else{
-            a->dir = insere(a->dir, v);
+        if(a->esq == NULL && a->dir == NULL){
+            
+            int i = 0;
+            while(a->c[i] == c[i] && i < 8){  //confusao de leves aq
+                i++;
+            }
+
+            int onde = strcmp(c[i], a->c[i]);  //se o onde = -1 , adiciona na esquerda, se for +1, adiciono na direita
+
+            Arvore* nova_folha = (Arvore*)malloc(sizeof(Arvore));
+            Arvore* nova_raiz = (Arvore*)malloc(sizeof(Arvore));
+
+            if(nova_folha){
+                *(nova_folha->c) = c;
+                nova_folha->tipo_no = 0;
+                nova_folha->esq = nova_folha->dir = NULL;
+            }
+
+            if(nova_raiz){
+                nova_raiz->tipo_no = 1;
+                nova_raiz->pos = i;  //qualquer coisa trocar por ++i
+            }
+
+            if(onde == -1){
+                nova_raiz->esq = nova_folha;
+                nova_raiz->dir = a;
+            }else{
+                nova_raiz->esq = a;
+                nova_raiz->dir = nova_folha;
+            }
+
         }
-        return a;
     }
 }
 
