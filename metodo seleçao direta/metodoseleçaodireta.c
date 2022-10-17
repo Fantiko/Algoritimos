0b9e359966fe7c9ebd8602bb5b51edd90b7696f2
Fantiko <86378890+Fantiko@users.noreply.github.com>
2022-10-17 14:10:19 -0300

    ATT selecaosort ADD .gitignore

--- metodo seleçao direta/metodoseleçaodireta.c
+++ metodo seleçao direta/metodoseleçaodireta.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #define MAX 5
 
-void ordenaSD(int vet[MAX]){
+void ordenaSD(int vet[]){
 int i, j, temp;
 
 for (i = 0; i < MAX; i++, j++){
@@ -16,12 +16,12 @@ for (i = 0; i < MAX; i++, j++){
 }
 }
 
-void MSD(int vet[MAX]){
+void MSD(int vet[]){
     int i, j, posmenor, aux;
 
     for  (i = 0; i < MAX; i++){
         posmenor=i;
-        for (j = i; j < MAX-1; j++){
+        for (j = i+1; j < MAX-1; j++){
             if (vet[j]< vet[posmenor]){
                 posmenor = j;
             }
@@ -33,15 +33,41 @@ void MSD(int vet[MAX]){
     
 }
 
+void selection_sort(int num[], int tam) { 
+  int i, j, min, aux;
+  for (i = 0; i < (tam-1); i++) 
+  {
+     min = i;
+     for (j = (i+1); j < tam; j++) {
+       if(num[j] < num[min]) 
+         min = j;
+     }
+     if (i != min) {
+       aux = num[i];
+       num[i] = num[min];
+       num[min] = aux;
+     }
+  }
+}
 
 
-int main(int argc, char const *argv[]){
+
+int main(){
     int vet[MAX] = {2,3,4,6,1};
-    ordenaSD(vet[MAX]);
-    for (int i = 0; i < MAX; i++)
+    
+    int j;
+    for(j = 0; j < MAX; j++)
     {
-        printf("vet[%d]: %d",i, vet[i]);
+        printf("vet[%d]: %d\n", j+1, vet[j]);
     }
+    printf("----------//----------\n");
+    selection_sort(vet, MAX);
+    int i;
+    for(i = 0; i < MAX; i++)
+    {
+        printf("vet[%d]: %d\n", i+1, vet[i]);
+    }
+    printf("----------//----------\n");
     
 
 
