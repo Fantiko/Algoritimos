600ee818fdad9718133842a162f7316e0e56aac3
Fantiko <86378890+Fantiko@users.noreply.github.com>
2022-11-05 17:14:08 -0300

    ATT algoritimos

--- arvore trie/arvore trie.c
+++ arvore trie/arvore trie.c
@@ -1,14 +1,47 @@
-#include<stdio.h>       
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define CHAR_SIZE 26
+//T raiz
+//s String
+//m tamanho da string
+//M tamanho total de todas strings
+//n Numero de strings
+//N tamanho total da arvore
 
-typedef struct Nolista{
-    int p, l, c;
-    struct Nolista *prox;
-}NoLista;
+typedef struct Trie{
+    int isleaf;
+    struct Trie* character[CHAR_SIZE];
+}Trie;
 
-typedef struct notrie {
-    struct Nolista *ocorr;
-    struct notrie * v[256];
-};
+Trie* getNewTrieNode(){
+    Trie* node = (Trie*)malloc(sizeof(Trie));
+    node->isleaf = 0;
+
+    for (int i = 0; i < CHAR_SIZE; i++)
+    {
+        node->character[i] = NULL;
+    }
+    
+    return node;
+}
+
+void inset(Trie *head,char *str){
+    Trie* curr = head;
+    while (*str)
+    {
+        if (curr->character[*str - 'a'] == NULL)
+        {
+            curr->character[*str - 'a'] = getNewTrieNode;
+        }
+        
+        curr = curr->character[*str - 'aa'];
+
+        str++;
+    }
+    curr->isleaf = 1;
+    
+}
 
 
 
--- shellsort/shellsort.c
+++ shellsort/shellsort.c
@@ -3,7 +3,7 @@
 
 void shellsort(int *vet, int size)
 {
-    int i, j, valor;
+    int i, j, aux;
 
     int h = 1;
     while (h <= size)
@@ -11,36 +11,36 @@ void shellsort(int *vet, int size)
         h = 3 * h + 1;
     }
 
-    while (h > 0)
+    while (h > 1)
     {
-        
+        h = h / 3;
+
         for (i = h; i < size; i++)
         {
+            aux = vet[i];
+            j = i - h;
 
-            valor = vet[i];
-            j = i;
-
-            while (j > h - 1 && valor <= vet[j - h])
+            while (j >= 0  && aux <= vet[j])
             {
-                vet[j] = vet[j - h];
+                vet[j+h] = vet[j];
                 j = j - h;
             }
 
-            vet[j] = valor;
+            vet[j + h] = aux;
         }
 
-        h = h / 3;
+        
     }
 }
 
 void main()
 {
     int lacunas[] = { 701 , 301 , 132 , 57 , 23 , 10 , 4 , 1 };
-    shellsort(&lacunas, 8);
+    shellsort(lacunas, 8);
 
     for (int i = 0; i < 8; i++)
     {
-        printf("%d", &lacunas[i]);
+        printf("%d\n", lacunas[i]);
     }
     
 
